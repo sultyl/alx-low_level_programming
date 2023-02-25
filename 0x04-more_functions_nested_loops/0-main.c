a39bc19ee16ef2e12e8ce956ea279c0b4a3880ff
Sultyl <adelekesultan85@gmail.com>
2023-02-24 20:00:13 -0800

    commit changes

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,7 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    /* Your code here */
+    char c;
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
     return (0);
 }
